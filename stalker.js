16184c5f42b225991aa06a1e84fe040e9e34dae4
luyong <luyong@nationalchip.com>
2018-09-03 14:01:58 +0800

    fix: 根据ecos stalker修改了登录方式，满足各种类型的load参数

--- stalker.js
+++ stalker.js
@@ -1,20 +1,17 @@
-
-
 var http = require('showtime/http');
 var fs = require('showtime/fs');
 var url = ' ';
-var	load_url = '';
-var	load_url1 = '/portal.php';
-var	load_url2 = '/stalker_portal/server/load.php';
+var server_url = ' ';
+var	load_url = ' ';
+var load_url1 = 'portal.php';
+var load_url2 = 'server/load.php';
 var refer = '/c/';
 var user_agent  = 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 4 rev: 1812 Mobile Safari/533.3';
 var timezone = 'Europe%2FUzhgorod';
 var ver_str = '&ver=ImageDescription:%200.2.18-r11-250;%20ImageDate:%20Wed%20Mar%2018%2017:54:59%20EET%202015;%20PORTAL%20version:%204.9.22;%20API%20Version:%20JS%20API%20version:%20331;%20STB%20API%20version:%20141;%20Player%20Engine%20version:%200x572';
 var mac = '00:1A:79:f5:75:d2';
 var mac_id = '001A79f575d2';
-var token;
 var global_responseData;
-var server_type = 0;
 
 (function(plugin) {
     var PLUGIN_PREFIX = "stalker:";
@@ -34,12 +31,13 @@ var server_type = 0;
         }
     }
     print(url);
+	server_url = url;
     var service = plugin.createService("stalker", PLUGIN_PREFIX+"start", "video", true, plugin.path + "stalker.png");
 
     plugin.addURI(PLUGIN_PREFIX+"start", function(page) {
         page.type = "directory";
 
-        token = handshake();
+        var token = handshake();
         responseData = get_genres(token);
         responseData = responseData.js;
         if(typeof(showtime.apiVersion) != "undefined" && showtime.apiVersion == "1.0.0")
@@ -85,7 +83,7 @@ var server_type = 0;
         }
         if(result == 0)
         {
-                page.appendItem(PLUGIN_PREFIX + 'playcmd:null', 'video',{title: "No Content"});
+            page.appendItem(PLUGIN_PREFIX + 'playcmd:null', 'video',{title: "No Content"});
         }
 	});
 
@@ -102,6 +100,7 @@ var server_type = 0;
                 total++;
             }
         }
+        print("total = " + total);
 
         function loader() {
             for(var i in global_responseData)
@@ -148,7 +147,7 @@ var server_type = 0;
         }
         else
         {
-            token = handshake();
+            var token = handshake();
             var s = creat_link(token,cmd).split(' ');
             url = s[0];
             if(s.length > 1)
@@ -168,11 +167,14 @@ var server_type = 0;
 })(this);
 
 function handshake1(){
-	//print('handshake1');
+	print('handshake1');
 	var responseText = '';
     var param = '?type=stb&action=handshake&JsHttpRequest=1-xml';
+	stalker_dec_server_url();
+
+	//print("url:" + url+load_url+param);
 	try{
-        var responseText = showtime.httpReq(url + load_url1 + param, {
+        var responseText = showtime.httpReq(url + load_url + param, {
                 headers: {
 			        'User-Agent' : user_agent,
 			        'Cookie' : 'mac=' + mac + '; stb_lang=en; timezone=' + timezone,
@@ -182,9 +184,7 @@ function handshake1(){
             }).toString();
         if(responseText.length>10)
         {
-            //print("handshake1:" + responseText);
-		    load_url = load_url1;
-		    server_type = 1;
+            print("handshake1:" + responseText);
         }
     }catch(error){
         print('handshake1 failed');
@@ -192,41 +192,59 @@ function handshake1(){
     return responseText;
 }
 
-function handshake2(){
-	//print('handshake2');
-	var responseText = '';
-    var param = '?type=stb&action=handshake&JsHttpRequest=1-xml';
-	try{
-        var responseText = showtime.httpReq(url + load_url2 + param, {
-                headers: {
-			        'User-Agent' : user_agent,
-			        'Cookie' : 'mac=' + mac + '; stb_lang=en; timezone=' + timezone,
-			        'Connection' : 'Keep-Alive',
-			        'X-User-Agent' : 'MAG250; Link: Ethernet',
-                },
-            }).toString();
-        if(responseText.length>10)
-        {
-            //print("handshake2:" + responseText);
-		    load_url = load_url2;
-		    server_type = 2;
-        }
-    }catch(error){
-        print('handshake2 failed');
-    }
-    return responseText;
+function stalker_dec_server_url(){
+	var len = 0;
+	var temp1 = '';
+	var temp2 = '';
+	var load = '';
+	load_url = '';
+	len = server_url.length;
+	//print("0:"+server_url);
+	if((len>0)&&(server_url.substring(len-1, len) == '/'))
+	{
+		server_url = server_url.substring(0,len-1);
+		  //print("1:"+server_url);
+	}
+
+	len = server_url.length;
+	if((len>1)&&(server_url.substring(len-2, len-1) == '/')&&(server_url.substring(len-1, len) == 'c'))
+	{
+		server_url = server_url.substring(0,len-2);
+		//print("2:"+server_url);
+	}
+	if(server_url.search("//") != -1)
+	{
+		temp1 = server_url.split("//")[1];
+		//print("temp1:"+temp1);
+		if(temp1.length > 0)
+		{
+			if(temp1.search("/") != -1)
+			{
+				temp2 = temp1.split("/",2);
+				len = temp2[1].length;
+				if(len > 0)
+				{
+					load_url = '/' + temp2[1] + '/' + load_url2;
+					url = 'http://' + temp2[0];
+
+				}
+			}
+		}
+	}
+	//print("load_url.length:"+load_url.length);
+	if(load_url.length == 0 )
+	{
+		load_url = '/' + load_url1;
+		url = server_url;
+	}
+
+	print("server_url:"+url);
+	print("load_url:"+load_url);
 }
 
 function handshake(){
     var responseText = '';
-    if((server_type == 0)||(server_type == 1))
-    {
-        responseText = handshake1();
-    }
-    if((server_type == 0)||(server_type == 2))
-    {
-        responseText = handshake2();
-    }
+    responseText = handshake1();
     var token = JSON.parse(responseText).js.token;
     get_profile(token);
     get_modules(token);
@@ -235,6 +253,8 @@ function handshake(){
 
 function get_profile(token){
     var param = '?type=stb&action=get_profile&hd=1'+ver_str+'&num_banks=2&sn=SN_'+mac_id+'&stb_type=MAG250&image_version=218&device_id=D1_'+mac_id+'&device_id2=D2_'+mac_id+'&signature=SG_'+mac_id+'&auth_second_step=0&hw_version=1.11-BD-00&not_valid_token=0&JsHttpRequest=1-xml';
+	print("get_profile");
+	//print("url:" + url+load_url+param);
     var responseText = showtime.httpReq(url + load_url + param, {
             headers: {
 			    'User-Agent' : user_agent,
@@ -250,6 +270,8 @@ function get_profile(token){
 
 function get_modules(token){
     var param = '?type=stb&action=get_modules&JsHttpRequest=1-xml';
+	print("get_modules");
+	//print("url:" + url+load_url+param);
     var responseText = showtime.httpReq(url + load_url + param, {
             headers: {
 			    'User-Agent' : user_agent,
@@ -265,6 +287,8 @@ function get_modules(token){
 
 function get_genres(token){
     var param = '?type=itv&action=get_genres&JsHttpRequest=1-xml';
+	print("get_genres");
+	//print("url:" + url+load_url+param);
     var responseText = showtime.httpReq(url + load_url + param, {
             headers: {
 			    'User-Agent' : user_agent,
@@ -282,6 +306,8 @@ function get_genres(token){
 
 function get_all_channels(token){
     var param = '?action=get_all_channels&type=itv&JsHttpRequest=1-xml';
+	print("get_all_channels");
+	//print("url:" + url+load_url+param);
     var responseText = showtime.httpReq(url + load_url + param, {
             headers: {
 			    'Authorization' : 'Bearer ' + token,
@@ -293,7 +319,7 @@ function get_all_channels(token){
 			    'Accept-Language' : 'en,*',
             },
         }).toString();
-    //print(responseText);
+    print("get_all_channels http request url: " + url + load_url + param);
     return JSON.parse(responseText).js.data;
 }
 
@@ -313,6 +339,8 @@ function creat_link(token,cmd){
 		enc_cmd = cmd;
 	}
     var param = '?type=itv&action=create_link&cmd='+enc_cmd+'&series=&forced_storage=&disable_ad=0&JsHttpRequest=1-xml';
+	print("creat_link");
+	//print("url:" + url+load_url+param);
     var responseText = showtime.httpReq(url + load_url + param, {
         headers: {
                 'User-Agent' : user_agent,
